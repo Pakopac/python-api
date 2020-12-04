7892b7a781d472d7833a4e1790e8355af92889a2
Lilian.Pacaud <Lilian.Pacaud@sky-real.com>
2020-12-04 18:04:49 +0100

    Fix test

--- algo.py
+++ algo.py
@@ -1,6 +1,31 @@
+import pandas as pd 
+import re
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.multiclass import OneVsRestClassifier
+from stop_words import get_stop_words
+from sklearn.svm import SVC
+import numpy as np
+from sklearn.model_selection import train_test_split
+from joblib import dump, load
+
+df = pd.read_csv("labels.csv") 
+
+df = pd.read_csv("labels.csv", usecols=['class', 'tweet'])
+df['tweet'] = df['tweet'].apply(lambda tweet: re.sub('[^A-Za-z]+', ' ', tweet.lower()))
+
+print(df.head())
+
 clf = make_pipeline(
     TfidfVectorizer(stop_words=get_stop_words('en')),
     OneVsRestClassifier(SVC(kernel='linear', probability=True))
 )
 
-dump(clf, 'filename.joblib') 
+clf.fit(X=df['tweet'], y=df['class'])
+
+print(clf)
+
+dump(clf, "filename.joblib") 
+clf = load('filename.joblib')
+
+print(clf)
